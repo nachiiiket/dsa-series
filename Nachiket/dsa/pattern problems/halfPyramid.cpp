fb62811e8b0b9bed1e0b9f7bf7ae3cd28b8d87c1
nachiiiket <yawalkarnachiket@gmail.com>
2022-07-15 02:50:19 +0530

    half pyramid

---
+++ Nachiket/dsa/pattern problems/halfPyramid.cpp
@@ -0,0 +1,22 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int n;
+    cin>>n;
+    int row = 1;
+    while(row <= n)
+    {
+        int column = 1;
+        while(column <= row)
+        {
+           cout<<"*";
+            column++;
+        }
+        cout<<endl;
+        row++;
+    }
+ 
+    return 0;
+}
